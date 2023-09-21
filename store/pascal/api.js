febc18acec2db2107d8f504f3cd163d0412a1d17
Crowdbotics <engineering@crowdbotics.com>
2023-09-21 06:07:31 +0000

    Update store.

--- store/pascal/api.js
+++ store/pascal/api.js
@@ -1,6 +1,11 @@
 import axios from "axios"
+import { PASCAL3_TOKEN } from "react-native-dotenv"
 const pascal = axios.create({
-  baseURL: "https://cbdash-pr-4907.herokuapp.com/dashboard",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
+  baseURL: "https://cbdash-pr-4907.herokuapp.com",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${PASCAL3_TOKEN}`
+  }
 })
 export const apiService = {}
